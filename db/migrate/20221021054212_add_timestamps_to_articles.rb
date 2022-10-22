7d4db328411a3059520c491ad1627516bb1845bd
codesmitty <varcodesmitty@gmail.com>
2022-10-22 11:40:28 -0400

    display articles

--- db/migrate/20221021054212_add_timestamps_to_articles.rb
+++ db/migrate/20221021054212_add_timestamps_to_articles.rb
@@ -1,6 +1,5 @@
 class AddTimestampsToArticles < ActiveRecord::Migration[7.0]
   def change
-    add_column :articles, :create_at, :updated_at, :datetime
-    add_column :articles, :created_at, :updatesd_at, :datetime
+    
   end
 end
